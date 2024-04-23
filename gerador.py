b591d64dc7c8cb4c357c4580d37a82bfae1ec48a
Talvez Seja o Felipe <84604925+felipe0c4@users.noreply.github.com>
2024-04-23 12:30:47 -0300

    🎭

--- gerador.py
+++ gerador.py
@@ -1,30 +1,19 @@
 import random
 
-alfabeto = [
+caracteres = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9","!", "@", "#", "$", "%",
+     "&", "*", "_", 
     ]
-
-
-numeros = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9"
-]
-
-simbs = [
-    "!", "@", "#", "$", "%", "&", "*", "_", 
-]
 
 
 lista = []
 
 
-for _ in range(10):
-    c = random.choice(alfabeto)
-    d = random.choice(numeros)
-    e = random.choice(simbs)
+for _ in range(16):
+    c = random.choice(caracteres)
     lista.append(c)
-    lista.append(e)
-    lista.append(d)
     
 string = ''.join(lista)
 
